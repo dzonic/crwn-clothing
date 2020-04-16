b7af1ecaa4e746a14a8305639cfdeab4113461a0
dzonic <nikolicnikol4466@.com>
2020-04-16 16:26:00 +0200

    breaking out homepage component into smaller components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Dirtectory from '../../components/directory/directory.component';
+
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Dirtectory />
+    </div>
+);
+
+export default HomePage;
