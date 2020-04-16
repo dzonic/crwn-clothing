6b9d809bf1b9991b2c826938567c6df8cd4d011f
dzonic <nikolicnikol4466@.com>
2020-04-16 14:18:57 +0200

    created project with create ract app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './homepage.component';
+
+function App() {
+  return (
+   <div>
+      <HomePage/>
+   </div>
+  );
+}
+
+export default App;
