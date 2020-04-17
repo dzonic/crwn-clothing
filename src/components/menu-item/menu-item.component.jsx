454859294fff5b79c7c9a6e9f23c7115bfcdce94
dzonic <nikolicnikol4466@.com>
2020-04-17 18:30:30 +0200

    adding header component and links to home, shop page and contact

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {withRouter} from 'react-router-dom';
 
 
-import './menu-item.styless.scss';
+import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match}) => (
   <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
