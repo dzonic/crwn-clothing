3990cda09ca4b91f54eae80e9351c1315b5c3a89
dzonic <nikolicnikol4466@.com>
2020-04-18 17:49:50 +0200

    added redux, and implemented userReducer and userActions to header and app components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import {auth} from '../../firebase/firebase.utils';
 
@@ -19,19 +20,23 @@ const Header = ({currentUser}) => (
         </Link>
         <Link className='option' to='/shop'>
           CONTACT
-        </Link>
-        {
-          currentUser ?
-          <div className='option' onClick={() => auth.signOut()}> 
-          SIGN OUT
-          </div>
-          :
-          <Link className='option' to='/signin'> 
-          SIGN IN
           </Link>
-        }
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className='option' to='/signin'>
+          SIGN IN
+        </Link>
+      )}
       </div>
     </div>
   );
   
-  export default Header;
+
+  const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+  });
+
+  export default connect(mapStateToProps)(Header);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import store from './redux/store';
+import {Provider} from 'react-redux';
+
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 
 
 ReactDOM.render(
+  <Provider store={store}>
     <BrowserRouter>
      <App />
-    </BrowserRouter>,
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
