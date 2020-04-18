dd68c324af9db8d697dd10daf5c7bd06749fc3b5
dzonic <nikolicnikol4466@.com>
2020-04-18 15:33:23 +0200

    impelemented utilis, included ability to store authenticated users into firestore db

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from'./pages/sign-in-sing-up/sign-in-sign-up.component';
 import Header from './components/header/header.component';
-import {auth} from './firebase/firebase.utils';
+import {auth,createUserProfileDocument} from './firebase/firebase.utils';
 
 
 
@@ -22,11 +22,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null ;
 
   componentDidMount() {
-    this.unsubscribeFromAuth =auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+    this.unsubscribeFromAuth =auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
-    })
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+
+        });
+        console.log(this.state);
+      }
+      this.setState({ currentUser: userAuth });
+    });
   }
 
   componentWillUnmount() {
--- src/pages/sign-in-sing-up/sign-in-sign-up.component.jsx
+++ src/pages/sign-in-sing-up/sign-in-sign-up.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
+
 import './sign-in-sign-up.styles.scss';
 
 const SignInAndSignUpPage= () => (
  <div className='sign-in-sign-up'> 
     <SignIn />
+    <SignUp />
   </div>
 );
 
