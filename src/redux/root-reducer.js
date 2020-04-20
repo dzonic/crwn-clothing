201240044149fdd68b445e9151b85d53fa682cee
dzonic <nikolicnikol4466@.com>
2020-04-20 17:27:15 +0200

    added redux in directory

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,17 +4,19 @@ import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
 
 
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['cart']
+  whitelist: ['cart']          
 };
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
