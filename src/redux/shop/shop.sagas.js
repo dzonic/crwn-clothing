09a33bdda6cd0634474272c718bbcbe29c654aec
dzonic <nikolicnikol4466@.com>
2020-04-24 21:54:38 +0200

    saved root-saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import {takeLatest, call, put} from 'redux-saga/effects';
+import {takeLatest, call, put, all} from 'redux-saga/effects';
 
 import {
     firestore,
@@ -32,4 +32,8 @@ export function* fetchCollectionsStart() {
       ShopActionTypes.FETCH_COLLECTIONS_START,
       fetchCollectionsAsync
     );
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)]);
 }
