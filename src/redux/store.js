1407b7ce2c6e1e043c20c98c527b7ba936467007
dzonic <nikolicnikol4466@.com>
2020-04-24 18:31:55 +0200

    implemented redux-saga in shop and store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,15 +2,21 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import {persistStore} from 'redux-persist';
 import rootReducer from './root-reducer';
-import thunk from 'redux-thunk';
 
-const middlewares = [thunk];
+import {fetchCollectionsStart} from './shop/shop.sagas';
+import createSagaMiddleware from 'redux-saga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
   }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(fetchCollectionsStart);
 
 export const persistor = persistStore(store);
 
