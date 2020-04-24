ec482fd5851be341003742c95c4bef3fead4c19c
dzonic <nikolicnikol4466@.com>
2020-04-24 20:56:14 +0200

    redux saga implemented on sign in with email

--- src/App.js
+++ src/App.js
@@ -11,10 +11,8 @@ import SignInAndSignUpPage from'./pages/sign-in-sing-up/sign-in-sign-up.componen
 import Header from './components/header/header.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 
-import {auth,createUserProfileDocument} from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
-//import {selectCollectionsForPreview} from './redux/shop/shop.selectors';
+
 
 
 
@@ -23,7 +21,7 @@ class App extends React.Component {
   unsubscribeFromAuth = null ;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    
 
     // this.unsubscribeFromAuth =auth.onAuthStateChanged(async userAuth => {
     //   if(userAuth) {
@@ -76,16 +74,7 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector ({
-  currentUser: selectCurrentUser,
-  //collectionsArray: selectCollectionsForPreview
+  currentUser: selectCurrentUser
 });
 
-
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default connect(mapStateToProps)(App);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,8 +3,7 @@ import {connect} from 'react-redux';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import {auth,signInWithGoogle} from '../../firebase/firebase.utils';
-import {googleSignInStart} from '../../redux/user/user.actions';
+import {googleSignInStart, emailSignInStart} from '../../redux/user/user.actions';
 
 import {
   SignInContainer,
@@ -24,15 +23,11 @@ class SignIn extends React.Component {
 
   handleSubmit = async event => {
     event.preventDefault();
-
+    const {emailSignInStart} = this.props;
     const { email, password } = this.state;
 
-    try {
-      await auth.signInWithEmailAndPassword(email, password);
-      this.setState({ email: '', password: '' });
-    } catch (error) {
-      console.log(error);
-    }
+    emailSignInStart(email, password);
+
   };
 
   handleChange = event => {
@@ -78,7 +73,8 @@ class SignIn extends React.Component {
 }
 
 const mapDispatchToProps = dispatch => ({
-  googleSignInStart: () => dispatch(googleSignInStart())
-})
+  googleSignInStart: () => dispatch(googleSignInStart()),
+  emailSignInStart: (email, password) => dispatch(emailSignInStart({ email, password }))
+});
 
 export default connect(null,mapDispatchToProps)(SignIn);
